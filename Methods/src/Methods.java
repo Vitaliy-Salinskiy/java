4933a646e7327c04beb6b2d65a8a3209500fb7a0
Vitaliy <148259238+Vitaliy-Salinskiy@users.noreply.github.com>
2024-05-18 22:32:12 +0200

    Math methods

--- Methods/src/Methods.java
+++ Methods/src/Methods.java
@@ -3,6 +3,8 @@ import java.util.List;
 
 public class Methods {
     public static void main(java.lang.String[] args) {
+
+//    !Strings Methods
       String name = "Ryan Gosling Gangster  ";
 
       System.out.println("Hello, " + name + "!");
@@ -25,6 +27,7 @@ public class Methods {
       System.out.println(name.isEmpty());
       System.out.println(name.concat("Gangster"));
 
+//    !Arrays Methods
       String[] arr1 = {"chicken", "bacon", "beef"};
       List<String> list =  Arrays.asList(arr1);
 
@@ -66,5 +69,23 @@ public class Methods {
 //      System.out.println(filteredArr);
       System.out.println(Arrays.toString(filteredArr));
 
+//    !Math Methods
+
+      double num1 = 15.6;
+      double num2 = 20.4;
+
+      System.out.println("Ceiling of " + num1 + ": " + Math.ceil(num1));
+      System.out.println("Ceiling of " + num2 + ": " + Math.ceil(num2));
+      System.out.println("Floor of " + num1 + ": " + Math.floor(num1));
+      System.out.println("Round " + num1 + ": " + Math.round(num1));
+      System.out.println("Round " + num2 + ": " + Math.round(num2));
+      System.out.println("Floor of " + num2 + ": " + Math.floor(num2));
+      System.out.println("Max of " + num1 + " and " + num2 + ": " + Math.max(num1, num2));
+      System.out.println("Min of " + num1 + " and " + num2 + ": " + Math.min(num1, num2));
+      System.out.println(2 + " to the power of " + 3 + ": " + Math.pow(2, 3));
+      System.out.println("Square root of " + 16 + ": " + Math.sqrt(16));
+
+      double randomValue = Math.floor(Math.random() * 10);
+      System.out.println("Random value: " + randomValue);
     }
 }
